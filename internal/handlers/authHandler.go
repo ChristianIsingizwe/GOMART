b9270cf58fbc49a4fd5e2e46475c8c921f2e3e71
Christian Isingizwe <bizimanaisingizwechristian@gmail.com>
2024-11-02 10:20:58 +0200

    Implementing roles

--- internal/handlers/authHandler.go
+++ internal/handlers/authHandler.go
@@ -1,7 +1,48 @@
 package handlers
 
-import "net/http"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/ChristianIsingizwe/GOMART/internal/database"
+	"github.com/ChristianIsingizwe/GOMART/internal/helpers"
+	"github.com/ChristianIsingizwe/GOMART/internal/models"
+	"github.com/ChristianIsingizwe/GOMART/internal/types"
+	"github.com/go-playground/validator/v10"
+)
 
 func RegisterUser(w http.ResponseWriter, r *http.Request){
-	
+
+	var validate *validator.Validate
+
+	var req types.RegisterRequest
+
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil{
+		http.Error(w, "Invalid request payload", http.StatusBadRequest)
+		return
+	}
+
+	err := validate.Struct(req)
+	if err != nil {
+		for _, err := range err.(validator.ValidationErrors){
+			fmt.Fprintf(w, "Validation failed for field '%s' : '%s\n'", err.Field(), err.Tag())
+		}
+		return 
+	}
+
+	var existingUser models.User
+
+	if err := database.DB.Where("email=?", req.Email).First(&existingUser).Error; err != nil{
+		http.Error(w, "User already exists", http.StatusConflict)
+		return 
+	}
+
+	hashedPassword, err := helpers.HashPassword(req.Password)
+	if err != nil {
+		http.Error(w, "Failed to hash the password", http.StatusInternalServerError)
+		return 
+	}
+
+
 }
--- internal/types/types.go
+++ internal/types/types.go
@@ -5,6 +5,7 @@ type RegisterRequest struct {
 	LastName  string `json:"lastName" validate:"required,min=3,max=80"`
 	Email     string `json:"email" validate:"required,email"`
 	Password  string `json:"password" validate:"required,strongpassword"`
+	Role      string `json:"role" validate:"omitempty,oneof=seller customer"`
 }
 
 type LoginRequest struct {
