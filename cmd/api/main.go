87032fad1b12564ffec30f6b209be8514f11b508
Christian Isingizwe <bizimanaisingizwechristian@gmail.com>
2024-11-11 03:23:46 +0200

    Initializing the server

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"net/http"
+	"os"
 
 	"github.com/ChristianIsingizwe/GOMART/internal/database"
+	"github.com/ChristianIsingizwe/GOMART/internal/handlers"
 )
 
 func main() {
@@ -11,5 +15,21 @@ func main() {
 	err := database.ConnectToDatabase()
 	if err != nil {
 		log.Fatalf("Could not connect to database.")
+	}
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/login", handlers.LoginUser)
+	mux.HandleFunc("/register", handlers.RegisterUser)
+
+
+	server := &http.Server{
+		Addr: os.Getenv("APP_PORT"),
+		Handler: mux,
+	}
+
+	fmt.Println("Starting server on port: " + os.Getenv("APP_PORT"))
+	if err := server.ListenAndServe(); err != nil {
+		log.Fatalf("Could not start the server: %s\n", err)
 	}
 }
