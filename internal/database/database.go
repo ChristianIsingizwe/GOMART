73d4eb96d216672790fd86a1c6165658b5260697
Christian Isingizwe <bizimanaisingizwechristian@gmail.com>
2024-11-14 08:53:57 +0200

    Some configurations optimised

--- internal/database/database.go
+++ internal/database/database.go
@@ -16,7 +16,7 @@ var DB *gorm.DB
 func ConnectToDatabase() error {
 	dsn := fmt.Sprintf(
 		"host=%s port=%s user=%s password=%s dbName=%s ",
-		os.Getenv("DB_HOST"),os.Getenv("PORT"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"),
+		os.Getenv("DB_HOST"),os.Getenv("DB_PORT"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"),
 	)
 
 	database, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
